c90097e1c333a72b6b4f6bc71fb9b19fb72f518b
Raphael Seher <raphael.seher@gmail.com>
2022-04-05 23:46:18 +0200

    Add sample for firebase data connection

--- app/src/main/java/at/aau/se2/gwent/MainActivity.java
+++ app/src/main/java/at/aau/se2/gwent/MainActivity.java
@@ -1,5 +1,14 @@
 package at.aau.se2.gwent;
 
+import java.util.ArrayList;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -19,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
+  private DatabaseReference databaseRef;
+  private ArrayList<String> messages = new ArrayList<>();
+
+  ValueEventListener postListener =
+      new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+          // Get Post object and use the values to update the UI
+          GenericTypeIndicator<ArrayList<String>> t =
+              new GenericTypeIndicator<ArrayList<String>>() {};
+          messages = dataSnapshot.getValue(t);
+          if (messages == null) {
+            messages = new ArrayList<>();
+          }
+          Log.v(TAG, "Messages: " + messages);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+          Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+        }
+      };
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,15 +69,25 @@ public class MainActivity extends AppCompatActivity {
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-            Log.v(TAG, "OnClick FAB");
+            messages.add("Hello, World!");
+            databaseRef.setValue(messages);
           }
         });
+
+    setupDatabaseReference();
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
+  }
+
+  private void setupDatabaseReference() {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    databaseRef = database.getReference("messages");
+    database.getReference("messages").addValueEventListener(postListener);
+    databaseRef.setValue(messages);
   }
 
   @Override
