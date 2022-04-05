b02a539c9d3bdb05ff61b5d230b4c58e6b3ffd09
Raphael Seher <raphael.seher@gmail.com>
2022-04-05 23:17:36 +0200

    Refactor to androidx
    Needed because of firebase dependency.

--- app/src/main/java/at/aau/se2/gwent/MainActivity.java
+++ app/src/main/java/at/aau/se2/gwent/MainActivity.java
@@ -1,14 +1,14 @@
 package at.aau.se2.gwent;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
