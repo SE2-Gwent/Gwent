f171aad7f9bc7c39a9e9026c5d5d4c3cf83c2b22
mavalo99 <101516960+mavalo99@users.noreply.github.com>
2022-04-07 18:53:56 +0200

    Update message

--- app/src/main/java/at/aau/se2/gwent/MainActivity.java
+++ app/src/main/java/at/aau/se2/gwent/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
           if (messages == null) {
             messages = new ArrayList<>();
           }
-          Log.v(TAG, "Messages: " + messages);
+          Log.v(TAG, "Messages: Max " + messages);
         }
 
         @Override
