a4ba108db603ee19e879edcb1497457e19067aae
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-04-23 16:18:58 +0200

    Register observer to update UI
    This commit registers an observer to the fragment

--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardFragment.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardFragment.java
@@ -5,49 +5,60 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 import at.aau.se2.gwent.databinding.DetailedCardFragmentBinding;
 
 public class DetailedCardFragment extends Fragment {
-  private static final String TAG = DetailedCardFragment.class.getSimpleName();
+    private static final String TAG = DetailedCardFragment.class.getSimpleName();
 
-  private DetailedCardViewModel viewModel;
-  private DetailedCardFragmentBinding binding;
+    private DetailedCardViewModel viewModel;
+    private DetailedCardFragmentBinding binding;
 
-  public static DetailedCardFragment newInstance() {
-    return new DetailedCardFragment();
-  }
+    public static DetailedCardFragment newInstance() {
+        return new DetailedCardFragment();
+    }
 
-  @Override
-  public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    viewModel =
-        new ViewModelProvider(this).get(DetailedCardViewModel.class); // link viewModel to fragment
-  }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel =
+                new ViewModelProvider(this).get(DetailedCardViewModel.class); // link viewModel to fragment
+        viewModel.getCurrentState().observe(this, this::updateUI);
+    }
 
-  @Override
-  public View onCreateView(
-      @NonNull LayoutInflater inflater,
-      @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    binding =
-        DetailedCardFragmentBinding.inflate(inflater, container, false); // parse detailed_card.xml
-    return binding.getRoot();
-  }
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        binding =
+                DetailedCardFragmentBinding.inflate(inflater, container, false); // parse detailed_card.xml
+        return binding.getRoot();
+    }
 
-  @Override
-  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-    // register onClickListener on ability button
-    binding.abilityButton.setOnClickListener(
-        view -> {
-          Log.v(TAG, "Ability activated.");
-        });
-  }
+        // register onClickListener on ability button
+        binding.abilityButton.setOnClickListener(
+                view -> {
+                    Log.v(TAG, "Ability activated.");
+                });
+    }
 
-  // TODO: Use the ViewModel
+    private void updateUI(DetailedCardViewModel.ViewState state) {
+        switch (state) {
+            case INITIAL:
+                // could show loading
+                break;
+            case LOADED:
+                // TODO: set the views according to the data within the viewModel
+        }
+    }
 }
--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
@@ -1,7 +1,27 @@
 package at.aau.se2.gwent.views.detailedcard;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DetailedCardViewModel extends ViewModel {
-  // TODO: Implement the ViewModel
+    // TODO: add var. card
+    // TODO: add var. img (which contains the artwork of the card)
+
+    private MutableLiveData<DetailedCardViewModel.ViewState> currentState = new MutableLiveData<>(DetailedCardViewModel.ViewState.INITIAL);
+
+    public MutableLiveData<DetailedCardViewModel.ViewState> getCurrentState() {
+        return currentState;
+    }
+
+    enum ViewState {
+        INITIAL,
+        LOADED
+    }
+
+    public DetailedCardViewModel() {
+        // TODO: init current card
+        // TODO: fetch img. by id of current card
+
+        currentState.setValue(ViewState.LOADED);
+    }
 }
