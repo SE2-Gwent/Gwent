b3c81c858140456bbc4a4bfa06612aa6adba6465
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-04-23 13:02:06 +0200

    Add base-functionality to DetailedCardFragment
    Bind the viewModel to the fragment, bind xml-file, set up onclicklistener on button

--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardFragment.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardFragment.java
@@ -1,37 +1,53 @@
 package at.aau.se2.gwent.views.detailedcard;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import at.aau.se2.gwent.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import at.aau.se2.gwent.databinding.DetailedCardFragmentBinding;
 
 public class DetailedCardFragment extends Fragment {
+  private static final String TAG = DetailedCardFragment.class.getSimpleName();
 
-    private DetailedCardViewModel viewModel;
+  private DetailedCardViewModel viewModel;
+  private DetailedCardFragmentBinding binding;
 
-    public static DetailedCardFragment newInstance() {
-        return new DetailedCardFragment();
-    }
+  public static DetailedCardFragment newInstance() {
+    return new DetailedCardFragment();
+  }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.detailed_card_fragment, container, false);
-    }
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    viewModel =
+        new ViewModelProvider(this).get(DetailedCardViewModel.class); // link viewModel to fragment
+  }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(DetailedCardViewModel.class);
-        // TODO: Use the ViewModel
-    }
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding =
+        DetailedCardFragmentBinding.inflate(inflater, container, false); // parse detailed_card.xml
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+
+    // register onClickListener on ability button
+    binding.abilityButton.setOnClickListener(
+        view -> {
+          Log.v(TAG, "Ability activated.");
+        });
+  }
+
+  // TODO: Use the ViewModel
 }
--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
@@ -3,5 +3,5 @@ package at.aau.se2.gwent.views.detailedcard;
 import androidx.lifecycle.ViewModel;
 
 public class DetailedCardViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+  // TODO: Implement the ViewModel
 }
