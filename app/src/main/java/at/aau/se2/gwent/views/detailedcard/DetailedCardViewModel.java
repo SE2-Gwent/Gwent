46e8bf82c147ddd9333a66c975619278713a87b2
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-04-24 14:07:00 +0200

    Init a test card within the viewModel

--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
@@ -1,27 +1,49 @@
 package at.aau.se2.gwent.views.detailedcard;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import at.aau.se2.gwent.data.Card;
 
 public class DetailedCardViewModel extends ViewModel {
-    // TODO: add var. card
-    // TODO: add var. img (which contains the artwork of the card)
+  private Card card;
 
-    private MutableLiveData<DetailedCardViewModel.ViewState> currentState = new MutableLiveData<>(DetailedCardViewModel.ViewState.INITIAL);
+  private MutableLiveData<DetailedCardViewModel.ViewState> currentState =
+      new MutableLiveData<>(DetailedCardViewModel.ViewState.INITIAL);
 
-    public MutableLiveData<DetailedCardViewModel.ViewState> getCurrentState() {
-        return currentState;
-    }
+  enum ViewState {
+    INITIAL,
+    LOADED
+  }
 
-    enum ViewState {
-        INITIAL,
-        LOADED
-    }
+  public DetailedCardViewModel() {
+    // TODO: retrieve card (card which was clicked by the user)
 
-    public DetailedCardViewModel() {
-        // TODO: init current card
-        // TODO: fetch img. by id of current card
+    // TODO: ----REMOVE-----
+    // init a sample card to test functionality
+    LinkedList<Card.CardType> types =
+        new LinkedList<>(Arrays.asList(Card.CardType.Human, Card.CardType.Knight));
+    this.card =
+        new Card(
+            42,
+            "Ard Feainn Crossbow Man",
+            types,
+            4,
+            "Deploy: Damage an enemy unit by 2. Barricade: Damage a random enemy unit by 1 whenever you play a soldier",
+            "Many thought that mounted crossbowman wouldn't work. Until they saw them in action.",
+            0);
+    // TODO: ------REMOVE------
 
-        currentState.setValue(ViewState.LOADED);
-    }
+    currentState.setValue(ViewState.LOADED);
+  }
+
+  public MutableLiveData<DetailedCardViewModel.ViewState> getCurrentState() {
+    return currentState;
+  }
+
+  public Card getCard() {
+    return card;
+  }
 }
