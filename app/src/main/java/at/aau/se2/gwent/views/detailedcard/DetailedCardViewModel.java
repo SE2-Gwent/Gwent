d0d2fc32bb111e452b2d09ba33ea7d4d91e28328
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-05-09 12:43:02 +0200

    Add string formater function to viewmodel
    This function is used to format the string name of card to reference the desired resource

--- app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
+++ app/src/main/java/at/aau/se2/gwent/views/detailedcard/DetailedCardViewModel.java
@@ -2,52 +2,65 @@ package at.aau.se2.gwent.views.detailedcard;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import at.aau.se2.gamelogic.models.Card;
 
 public class DetailedCardViewModel extends ViewModel {
-    private Card card;
-    private String imgResourceName;
+  private Card card;
+  private String imgResourceName;
 
-    private MutableLiveData<DetailedCardViewModel.ViewState> currentState =
-            new MutableLiveData<>(DetailedCardViewModel.ViewState.INITIAL);
+  private MutableLiveData<DetailedCardViewModel.ViewState> currentState =
+      new MutableLiveData<>(DetailedCardViewModel.ViewState.INITIAL);
 
-    enum ViewState {
-        INITIAL,
-        LOADED
-    }
+  enum ViewState {
+    INITIAL,
+    LOADED
+  }
 
-    public DetailedCardViewModel() {
+  public DetailedCardViewModel() {
 
-        // TODO: remove dummy card
-        int id = 0;
-        String name = "Ard Feainn Crossbow Man";
-        int power = 3;
-        int powerDiff = 0;
-        String cardText =
-                "Deploy: Damage an enemy unit by 2. Barricade: Damage a random enemy unit by 1 whenever you play a soldier.";
-        Card testCard = new Card(id, name, power, powerDiff, cardText, null);
-        this.card = testCard;
-    /*
-    TODO:
-    here we need a unique name which refers to the cardImage (large used for detailed card view)
-    i would suggest to use the cardName + postfix (large) (for this we would need a small function
-    which removes spaces from name and appends "large" to it afterwards
-     */
-        imgResourceName = "detailed_card_test_to_load";
+    // TODO: remove dummy card
+    int id = 0;
+    String name = "Ard Feainn Crossbow Man";
+    int power = 3;
+    int powerDiff = 0;
+    String cardText =
+        "Deploy: Damage an enemy unit by 2. Barricade: Damage a random enemy unit by 1 whenever you play a soldier.";
+    Card testCard = new Card(id, name, power, powerDiff, cardText, null);
+    this.card = testCard;
 
-        currentState.setValue(ViewState.LOADED);
-    }
+    setImgResourceName();
 
-    public MutableLiveData<DetailedCardViewModel.ViewState> getCurrentState() {
-        return currentState;
-    }
+    currentState.setValue(ViewState.LOADED);
+  }
 
-    public Card getCard() {
-        return card;
-    }
+  public MutableLiveData<DetailedCardViewModel.ViewState> getCurrentState() {
+    return currentState;
+  }
 
-    public String getImgResourceName() {
-        return imgResourceName;
-    }
+  public Card getCard() {
+    return card;
+  }
+
+  public String getImgResourceName() {
+    return imgResourceName;
+  }
+
+  /*
+  this function is used to set the resource name of the image to load
+  we reference resources according to detailed-card-view by taking the name of the card
+  first we replace all spaces by an underscore
+  second we convert the string to lowercase
+  third we append '_detail to it'
+
+  for example:
+  Ard Feainn Crossbow Man -> ard_feainn_crossbow_man_detail
+   */
+  private void setImgResourceName() {
+    String imageResourceName = "";
+
+    imageResourceName += card.getName().replaceAll(" ", "_").toLowerCase();
+    imageResourceName += "_detail";
+
+    this.imgResourceName = imageResourceName;
+  }
 }
