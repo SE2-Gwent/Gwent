ec0f656134ed827d5e7d1a0e71cc0fb2ac2884c6
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-05-09 17:05:46 +0200

    Add missing getter/setter to dto (Card)

--- gamelogic/src/main/java/at/aau/se2/gamelogic/models/Card.java
+++ gamelogic/src/main/java/at/aau/se2/gamelogic/models/Card.java
@@ -7,86 +7,96 @@ import java.util.Map;
 import at.aau.se2.gamelogic.models.cardactions.ActionParams;
 
 public class Card {
-  private int id;
-  private String name;
-  private ArrayList<CardType> types;
-  private int power;
-  private int powerDiff;
-  private String cardText;
-  private ArrayList<ActionParams> cardActions;
-  private Map<ActionParams, Integer> currentActionCooldown;
+    private int id;
+    private String name;
+    private ArrayList<CardType> types;
+    private int power;
+    private int powerDiff;
+    private String cardText;
+    private ArrayList<ActionParams> cardActions;
+    private Map<ActionParams, Integer> currentActionCooldown;
 
-  public Card(
-      int id,
-      String name,
-      ArrayList <CardType> types,
-      int power,
-      int powerDiff,
-      String cardText,
-      ArrayList<ActionParams> cardActions) {
-    this.id = id;
-    this.name = name;
-    this.types = types;
-    this.power = power;
-    this.powerDiff = powerDiff;
-    this.cardText = cardText;
-    this.cardActions = cardActions;
-    this.currentActionCooldown = new HashMap<>();
-  }
+    public Card(
+            int id,
+            String name,
+            ArrayList<CardType> types,
+            int power,
+            int powerDiff,
+            String cardText,
+            ArrayList<ActionParams> cardActions) {
+        this.id = id;
+        this.name = name;
+        this.types = types;
+        this.power = power;
+        this.powerDiff = powerDiff;
+        this.cardText = cardText;
+        this.cardActions = cardActions;
+        this.currentActionCooldown = new HashMap<>();
+    }
 
-  public int getId() {
-    return id;
-  }
 
-  public void setId(int id) {
-    this.id = id;
-  }
 
-  public String getName() {
-    return name;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public int getPower() {
-    return power;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setPower(int power) {
-    this.power = power;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public int getPowerDiff() {
-    return powerDiff;
-  }
+    public ArrayList<CardType> getTypes() {
+        return types;
+    }
 
-  public void setPowerDiff(int powerDiff) {
-    this.powerDiff = powerDiff;
-  }
+    public void setTypes(ArrayList<CardType> types) {
+        this.types = types;
+    }
 
-  public String getCardText() {
-    return cardText;
-  }
+    public int getPower() {
+        return power;
+    }
 
-  public void setCardText(String cardText) {
-    this.cardText = cardText;
-  }
+    public void setPower(int power) {
+        this.power = power;
+    }
 
-  public ArrayList<ActionParams> getCardActions() {
-    return cardActions;
-  }
+    public int getPowerDiff() {
+        return powerDiff;
+    }
 
-  public void setCardActions(ArrayList<ActionParams> cardActions) {
-    this.cardActions = cardActions;
-  }
+    public void setPowerDiff(int powerDiff) {
+        this.powerDiff = powerDiff;
+    }
 
-  public Map<ActionParams, Integer> getCurrentActionCooldown() {
-    return currentActionCooldown;
-  }
+    public String getCardText() {
+        return cardText;
+    }
 
-  public void setCurrentActionCooldown(Map<ActionParams, Integer> currentActionCooldown) {
-    this.currentActionCooldown = currentActionCooldown;
-  }
+    public void setCardText(String cardText) {
+        this.cardText = cardText;
+    }
+
+    public ArrayList<ActionParams> getCardActions() {
+        return cardActions;
+    }
+
+    public void setCardActions(ArrayList<ActionParams> cardActions) {
+        this.cardActions = cardActions;
+    }
+
+    public Map<ActionParams, Integer> getCurrentActionCooldown() {
+        return currentActionCooldown;
+    }
+
+    public void setCurrentActionCooldown(Map<ActionParams, Integer> currentActionCooldown) {
+        this.currentActionCooldown = currentActionCooldown;
+    }
 }
