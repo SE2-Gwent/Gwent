8047ad8ba1d2ccf64cd14c8950239ba1e3770d8d
Raphael Seher <raphael.seher@gmail.com>
2022-05-17 15:07:15 +0200

    Change lastActions to just return the last action send

--- gamelogic/src/main/java/at/aau/se2/gamelogic/comunication/SyncRoot.java
+++ gamelogic/src/main/java/at/aau/se2/gamelogic/comunication/SyncRoot.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
-import com.google.firebase.database.Exclude;
-
 import androidx.annotation.Keep;
 import at.aau.se2.gamelogic.models.GameField;
 
@@ -13,9 +11,6 @@ public class SyncRoot {
   private String createdOn = new Date().toString();
   private HashMap<String, SyncAction> syncActions = new HashMap<>(); // firebase needs String keys
   private GameField gameField;
-
-  // not synced
-  @Exclude private int lastSyncActionSize = 0;
 
   @Keep
   public SyncRoot() {}
@@ -29,12 +24,12 @@ public class SyncRoot {
 
   public ArrayList<SyncAction> getLastActions() {
     ArrayList<SyncAction> lastActions = new ArrayList<>();
+    if (syncActions.isEmpty()) return lastActions;
 
-    for (int i = lastSyncActionSize; i < syncActions.size(); i++) {
+    for (int i = syncActions.size() - 1; i < syncActions.size(); i++) {
       lastActions.add(syncActions.get(i + "_key"));
     }
 
-    lastSyncActionSize = syncActions.size();
     return lastActions;
   }
 
