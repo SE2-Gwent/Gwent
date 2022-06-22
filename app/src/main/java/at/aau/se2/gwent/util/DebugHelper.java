e6a42d79828e997e6562eb21f3fa359820c33608
Raphael Seher <raphael.seher@gmail.com>
2022-06-22 10:06:32 +0200

    Remove commented code

--- app/src/main/java/at/aau/se2/gwent/util/DebugHelper.java
+++ app/src/main/java/at/aau/se2/gwent/util/DebugHelper.java
@@ -8,22 +8,5 @@ public class DebugHelper {
 
   public static ArrayList<Card> generateTestCards() throws Exception {
     return DeckGeneration.generateCardDeck();
-    /*ArrayList<Card> testCards = new ArrayList<>();
-    for (int i = 1; i <= 25; i++) {
-      testCards.add(
-          new Card(
-              i,
-              "TestCard " + i,
-              new ArrayList<CardType>(),
-              i,
-              0,
-              "This is a test Card",
-              null,
-              null,
-              "rm_ard_feainn_crossbow_man_basic",
-              "rm_ard_feainn_crossbow_man_detail"));
-    }
-
-    return testCards;*/
   }
 }
